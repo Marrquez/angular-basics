14a6dd77d9254a9588c083fbd48aef6371311107
Marrquez <warrdnez@gmail.com>
2023-11-14 23:25:55 +0100

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, LeftSidebarComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  providers: [
+    provideAnimations()
+  ]
 })
 export class AppComponent {
   title = 'angular-basics';
--- src/app/components/decorators/decorators.component.ts
+++ src/app/components/decorators/decorators.component.ts
@@ -1,19 +1,39 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-decorators',
   standalone: true,
   imports: [CommonModule, RouterModule],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        backgroundColor: 'yellow',
+        border: '3px solid yellow'
+      })),
+      state('closed', style({
+        backgroundColor: 'red',
+        border: '3px solid red'
+      })),
+      transition('open => closed', [animate('2s')]),
+      transition('open => closed', [animate('2s')])
+    ])
+  ], 
+  providers: [
+    provideAnimations()
+  ],
   templateUrl: './decorators.component.html',
   styleUrl: './decorators.component.sass'
 })
 export class DecoratorsComponent implements OnInit {
   @HostBinding('style.--width-of-the-square')
-  squareWidth = '270px';
+  squareWidth = '170px';
+  newWidth = 170;
 
-  newWidth = 270;
+  isOpen = false;
 
   ngOnInit () {
     
@@ -22,5 +42,9 @@ export class DecoratorsComponent implements OnInit {
   updateWidth(event: Event): void {
     const newWidth = (event.target as HTMLInputElement).value;
     this.squareWidth = newWidth + 'px';
+  }
+
+  toggleColor():void {
+    this.isOpen = !this.isOpen;
   }
 }
