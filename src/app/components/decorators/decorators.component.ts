65e216a70560569e86112d14b4f38cc2e384fbd3
Marrquez <warrdnez@gmail.com>
2023-11-15 10:25:58 +0100

    viewchild decorator

--- src/app/components/decorators/decorators.component.ts
+++ src/app/components/decorators/decorators.component.ts
@@ -1,12 +1,26 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit, Input, ViewChild, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
+  selector: 'menu-item', 
+  standalone: true,
+  template: `<p (click)='updateText()'>The text: {{menuText}}</p>`
+})
+export class MenuItemComponent {
+  @Input() menuText: string = 'Default menu text';
+  @Output() updateTextEvent = new EventEmitter();
+
+  updateText(): void {
+    this.updateTextEvent.emit('Update from child');
+  }
+}
+
+@Component({
   selector: 'app-decorators',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MenuItemComponent], 
   animations: [
     trigger('openClose', [
       state('open', style({
@@ -30,6 +44,10 @@ export class DecoratorsComponent implements OnInit {
   @HostBinding('style.--width-of-the-square')
   squareWidth = '150px';
   newWidth = 150;
+  menuItemText = 'This is the initial text';
+  count = 0;
+
+  @ViewChild('MenuItemComponent') childMenuItem!: MenuItemComponent;
 
   isOpen = false;
 
@@ -44,5 +62,15 @@ export class DecoratorsComponent implements OnInit {
 
   toggle():void {
     this.isOpen = !this.isOpen;
+  }
+
+  changeText():void {
+    this.childMenuItem.menuText = 'Updated using @viewchild: '+ this.count;
+    this.count++;
+  }
+
+  updateText(): void {
+    this.childMenuItem.menuText = 'Updated via @output: '+ this.count;
+    this.count++;
   }
 }
