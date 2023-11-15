ee323785d15663e1a158a8b036cd4217cbe1fe84
Marrquez <warrdnez@gmail.com>
2023-11-15 12:14:13 +0100

    pipes

--- src/app/components/ag-forms/ag-forms.component.ts
+++ src/app/components/ag-forms/ag-forms.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { ReactiveFormsModule, FormControl, FormsModule } from '@angular/forms';
+import { ExponentialStrengthPipe } from '../../pipes/exponential-strength.pipe';
 
 @Component({
   selector: 'app-ag-forms',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, DatePipe, ExponentialStrengthPipe],
   templateUrl: './ag-forms.component.html',
   styleUrl: './ag-forms.component.sass'
 })
-export class AgFormsComponent {
+export class AgFormsComponent {  
   favoriteColorControl = new FormControl();
   secondFavoriteColor = '';
+  myBday = new Date(1989, 12, 25);
+ 
+  get fcValue():number {
+    return this.favoriteColorControl.value;
+  }
 }
