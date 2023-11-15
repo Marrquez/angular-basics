7ad51b5d540437032446497fb2f307ef4255b859
Marrquez <warrdnez@gmail.com>
2023-11-15 09:57:12 +0100

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, LeftSidebarComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
-  providers: [
-    provideAnimations()
-  ]
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   title = 'angular-basics';
--- src/app/components/decorators/decorators.component.ts
+++ src/app/components/decorators/decorators.component.ts
@@ -2,7 +2,6 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-decorators',
@@ -11,27 +10,26 @@ import { provideAnimations } from '@angular/platform-browser/animations';
   animations: [
     trigger('openClose', [
       state('open', style({
-        backgroundColor: 'yellow',
-        border: '3px solid yellow'
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'red'
       })),
       state('closed', style({
-        backgroundColor: 'red',
-        border: '3px solid red'
+        height: '100px',
+        opacity: 0.8,
+        backgroundColor: 'orange'
       })),
-      transition('open => closed', [animate('2s')]),
-      transition('open => closed', [animate('2s')])
-    ])
+      transition('open => closed', [animate('.6s')]),
+      transition('closed => open', [animate('.6s')])
+    ]) 
   ], 
-  providers: [
-    provideAnimations()
-  ],
   templateUrl: './decorators.component.html',
   styleUrl: './decorators.component.sass'
 })
 export class DecoratorsComponent implements OnInit {
   @HostBinding('style.--width-of-the-square')
-  squareWidth = '170px';
-  newWidth = 170;
+  squareWidth = '150px';
+  newWidth = 150;
 
   isOpen = false;
 
@@ -44,7 +42,7 @@ export class DecoratorsComponent implements OnInit {
     this.squareWidth = newWidth + 'px';
   }
 
-  toggleColor():void {
+  toggle():void {
     this.isOpen = !this.isOpen;
   }
 }
